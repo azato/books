aca9ca6aa4d5e51519ef860cfc2ee830a02f14ef
Álvaro Zato <lvaro Zato>
2017-03-05 14:22:22 +0100

    Initial commit

---
+++ src/main/java/helmantico/App.java
@@ -0,0 +1,13 @@
+package helmantico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
